f2744b669cbd0c4ad5e364f33e12c63eb6e69ac8
jrm246 <jrm246@att.net>
2021-07-28 00:40:11 -0400

    Test

--- StoreAppData/Database.cs
+++ StoreAppData/Database.cs
@@ -4,22 +4,20 @@ using Microsoft.Extensions.Configuration;
 
 namespace StoreAppData
 {
-    class DatabaseConnection
+    public class DatabaseConnection
     {
+        public DbContextOptions<StoreAppDBContext> Options { get; set; }
+
+        public static DatabaseConnection DatabaseConnectionOption { get; set; }
+
+        public DatabaseConnection(DbContextOptions<StoreAppDBContext> option)
+        {
+            Options = option;
+        }
         public static DbContextOptions<StoreAppDBContext> GetDatabaseOptions() 
         {
-            //Get the configuration from our appsetting.json file
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json")
-                .Build();
 
-            //Grabs our connectionString from our appsetting.json
-            string connectionString = configuration.GetConnectionString("Reference2DB");
-
-            DbContextOptions<StoreAppDBContext> options = new DbContextOptionsBuilder<StoreAppDBContext>()
-                .UseSqlServer(connectionString)
-                .Options;
+            DbContextOptions<StoreAppDBContext> options = new DbContextOptions<StoreAppDBContext>();
 
             return options;
         }
